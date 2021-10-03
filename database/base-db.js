0081acad278ad8f2d482d4896fdb9dae03f2b241
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-10-04 00:01:07 +0300

    Make baseDB completely async

--- database/base-db.js
+++ database/base-db.js
@@ -1,65 +1,53 @@
 const fs = require('fs')
-const { resolve } = require('path')
-
 
 class BaseDatabase {
   constructor(model) {
     this.model = model
-    this.filename = model.name.toLowerCase()
-  }
-
-  save(objects) {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(`./database/data/${this.filename}.json`, JSON.stringify(objects), (err) => {
-        if (err) return reject(err)
-        resolve()
-      })
-    })    
+    this.path = `./${model.name.toLowerCase()}.json`
   }
   
-  load () {
-    return new Promise((resolve, reject) => {
-      if (!fs.existsSync(`./database/data/${this.filename}.json`))
-        fs.writeFileSync(`./database/data/${this.filename}.json`, '[]')
-
-      const file = fs.readFile(`./database/data/${this.filename}.json`, 'utf-8', (err, file) => {
+  save (objects) {
+    return new Promise((resolve, reject)=>{
+      fs.writeFile(this.path, JSON.stringify(objects), (err) => {
         if (err) reject(err)
-        const objects = JSON.parse(file)
+        resolve()
+      })
+    })
+  }
+
+  load () {
+    return new Promise((resolve, reject)=>{
+      fs.readFile(this.path, (err, data) => {
+        if (err) reject (err)
+        const objects = JSON.parse(data)
         resolve(objects.map(this.model.create))
       })
     })
   }
-  
+
   async insert (object) {
     const objects = await this.load()
-    objects.concat(Object)
-    await this.save(objects)
-  }
-  
-  remove (index) {
-    this.load().then((objects) => {
-      objects.splice(index, 1)
-    }).then(() => {
-      objects.save().then(() => {console.log('remove')})
-    })
+    objects.push(object)
+    return this.save(objects)
   }
 
   async findById (id) {
     const objects = await this.load()
-    const objIdx = objects.findIndex(obj => obj.id == id)
+    const object = objects.find(o => o.id == id)
 
-    if (objIdx < 0)
-      return null
-    return objects[objIdx]
+    if (object == undefined)
+      throw Error(`Object with id ${id} is not found.`)
+    return object
   }
 
   async update (object) {
     const objects = await this.load()
-    const idx = objects.findIndex(obj => obj.id == object.id)
-    
-    if (idx == -1) throw new Error(`Cannot find ${this.model.name} with id ${object.id}`)
-    
-    objects.splice(idx, 1, object)
+    const objectIdx = objects.findIndex(o => o.id == object.id)
+
+    if (objectIdx < 0)
+      throw Error(`Object with id ${object.id} is not found.`)
+
+    objects.splice(objectIdx, 1, object)
     return this.save(objects)
   }
 }
