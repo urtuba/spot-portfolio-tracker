61b4f3320e72331214d4b2c979cf36f21f671428
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-10-07 02:31:12 +0300

    Seperation of data generation scenario from index.js
    #4

--- index.js
+++ index.js
@@ -1,69 +1,64 @@
 const Investor = require('./models/investor')
 const Asset = require('./models/asset')
-const PortfolioManager = require('./models/portfolio-manager')
 
 const assetDatabase = require('./database/asset-db')
 const investorDatabase = require('./database/investor-db')
-const portfolioManagerDb = require('./database/portfolio-manager-db')
+const walletDb = require('./database/wallet-db')
 const transactionDatabase = require('./database/transaction-db')
+
+const { createTestData } = require('./test-data')
+const investorDb = require('./database/investor-db')
 
 
 async function main()
 {
-    // create supported assets and save them
-    const btc = new Asset(undefined, 'Bitcoin', 'BTC', Asset.types.COIN)
-    const eth = new Asset(undefined, 'Ethereum', 'ETH', Asset.types.COIN)
-    const usd = new Asset(undefined, 'Dollar', 'USD', Asset.types.FIAT)
-    const tl = new Asset(undefined, 'Turkish Lira', 'TRY', Asset.types.FIAT)
-    await assetDatabase.save([btc, eth, usd, tl])
-    // console.log(await(assetDatabase.load()))
-
-
-    // create a user and use assets into db
-    const samed = new Investor(undefined, 'Samed', 'samed@tutanota.com', 'password')
+    await createTestData()
     
-    samed.addFavorite(await assetDatabase.findByName('Bitcoin'))
-    samed.addFavorite(await assetDatabase.findByName('Ethereum'))
-    samed.removeFavorite(await assetDatabase.findByName('Bitcoin'))
+    const assets = await assetDatabase.load()
+    const investors = await investorDatabase.load()
 
+    console.log('LOADED : ' + assets.length + ' Assets')
+    console.log('LOADED : ' + investors.length + ' Investors')
+    
+    console.log(investors[1].wallets[0])
 
+    
     // create new portfolio managers, then save them into db
-    const samedLongTerm = new PortfolioManager(undefined, 'Long Term Portfolio')
-    const someonesPortfolio = new PortfolioManager(undefined, 'Some Portfolio')
-    await someonesPortfolio.addAsset(await assetDatabase.findByName('Bitcoin', 12, 34000))
-    const samedMainWallet = samed.wallets[0]
-    await samedMainWallet.addAsset(await assetDatabase.findByName('Turkish Lira'), 1500, 0.13)
-    await portfolioManagerDb.save([samedLongTerm, someonesPortfolio, samedMainWallet])
+    // const samedLongTerm = new PortfolioManager(undefined, 'Long Term Portfolio')
+    // const someonesPortfolio = new PortfolioManager(undefined, 'Some Portfolio')
+    // await someonesPortfolio.addAsset(await assetDatabase.findByName('Bitcoin', 12, 34000))
+    // const samedMainWallet = samed.wallets[0]
+    // await samedMainWallet.addAsset(await assetDatabase.findByName('Turkish Lira'), 1500, 0.13)
+    // await portfolioManagerDb.save([samedLongTerm, someonesPortfolio, samedMainWallet])
     // console.log(await portfolioManagerDb.load())
 
 
     // add new users, then save them into db
-    const armagan = new Investor(undefined, 'Armagan', 'xyz@nimble.dev', 'pwd')
-    const omer = new Investor(undefined, 'Omer', 'omer@gmail.com', 'pwd')
-    await investorDatabase.save([samed, armagan, omer])
+    // const armagan = new Investor(undefined, 'Armagan', 'xyz@nimble.dev', 'pwd')
+    // const omer = new Investor(undefined, 'Omer', 'omer@gmail.com', 'pwd')
+    // await investorDatabase.save([samed, armagan, omer])
     // console.log(await investorDatabase.load())
 
 
     // update users in database
-    omer.password = 'omer123'
-    await investorDatabase.update(omer)
-    armagan.wallets.push(someonesPortfolio)
-    await investorDatabase.update(armagan)
-    samed.wallets.push(samedMainWallet)
-    await investorDatabase.update(samed)
+    // omer.password = 'omer123'
+    // await investorDatabase.update(omer)
+    // armagan.wallets.push(someonesPortfolio)
+    // await investorDatabase.update(armagan)
+    // samed.wallets.push(samedMainWallet)
+    // await investorDatabase.update(samed)
     // console.log(await investorDatabase.load())
     
 
 
     // make some transactions
-    const samedMainPortfolio = samed.wallets[0]
-    await samedMainPortfolio.addAsset(await assetDatabase.findByName('Bitcoin'), 1, 40500)
-    await samedMainPortfolio.sellAsset(await assetDatabase.findByName('Bitcoin'), 0.5, 43000)
+    // const samedMainPortfolio = samed.wallets[0]
+    // await samedMainPortfolio.addAsset(await assetDatabase.findByName('Bitcoin'), 1, 40500)
+    // await samedMainPortfolio.sellAsset(await assetDatabase.findByName('Bitcoin'), 0.5, 43000)
 
     // console.log(samedMainPortfolio)
-    // console.log(await samedMainPortfolio.getTransactions()) 
-
-
+    // console.log(samedMainPortfolio.getTransactions) 
+    // console.log(await transactionDatabase.load())
 }
 
 main()
@@ -71,14 +66,4 @@ main()
 
 
     
-
-
-
-
-
-
-
-
-
-
 
---
+++ test-data.js
@@ -0,0 +1,40 @@
+const assetDb = require('./database/asset-db')
+const investorDb = require('./database/investor-db')
+const Asset = require('./models/asset')
+const Investor = require('./models/investor')
+const purchaseManager = require('./models/purchase-manager')
+const Wallet = require('./models/wallet')
+
+const createTestData = async () => {
+  // create supported assets and save them
+  const btc = new Asset(undefined, 'Bitcoin', 'BTC', Asset.types.COIN)
+  const eth = new Asset(undefined, 'Ethereum', 'ETH', Asset.types.COIN)
+  const usd = new Asset(undefined, 'Dollar', 'USD', Asset.types.FIAT)
+  const tl = new Asset(undefined, 'Turkish Lira', 'TRY', Asset.types.FIAT)
+  await assetDb.save([btc, eth, usd, tl])
+
+  // add new users
+  const samed = new Investor(undefined, 'Samed', 'samed@tutanota.com', 'password')
+  const armagan = new Investor(undefined, 'Armagain', 'armagan@nimble.dev', 'password')
+  samed.addFavorite(btc)
+  samed.addFavorite(tl)
+  armagan.addFavorite(usd)
+  await investorDb.save([samed, armagan])
+
+  // update users in database
+  samed.password = 'samed123'
+  samed.removeFavorite(btc)
+  const samedLongTerm = new Wallet(undefined, 'Long Term Wallet')
+  samed.wallets.push(samedLongTerm)
+  await investorDb.update(samed)
+
+  // use purchase manager to manage wallets
+  const armaganMainWallet = armagan.wallets[0]
+  await purchaseManager.addAsset(armaganMainWallet, btc, 15, 50000)
+  await purchaseManager.addAsset(armaganMainWallet, usd, 100000, 1)
+  await purchaseManager.removeAsset(armaganMainWallet, btc, 55000, true)
+  await investorDb.update(armagan)
+
+}
+
+module.exports = { createTestData }
