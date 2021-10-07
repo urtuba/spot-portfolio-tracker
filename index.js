49bd621c789919eef636262f637e783cbad16353
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-10-07 03:34:25 +0300

    Add convertor for tx id arrays to tx arrays, shorten main
    #2 #4

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const walletDb = require('./database/wallet-db')
 const transactionDatabase = require('./database/transaction-db')
 
 const { createTestData } = require('./test-data')
+const { resetDb } = require('./reset-db')
 const investorDb = require('./database/investor-db')
+const transactionDb = require('./database/transaction-db')
+const { loadTransactions } = require('./lib/utils')
 
 
 async function main()
 {
+    await resetDb()
     await createTestData()
     
     const assets = await assetDatabase.load()
@@ -20,45 +24,8 @@ async function main()
     console.log('LOADED : ' + assets.length + ' Assets')
     console.log('LOADED : ' + investors.length + ' Investors')
     
-    console.log(investors[1].wallets[0])
-
-    
-    // create new portfolio managers, then save them into db
-    // const samedLongTerm = new PortfolioManager(undefined, 'Long Term Portfolio')
-    // const someonesPortfolio = new PortfolioManager(undefined, 'Some Portfolio')
-    // await someonesPortfolio.addAsset(await assetDatabase.findByName('Bitcoin', 12, 34000))
-    // const samedMainWallet = samed.wallets[0]
-    // await samedMainWallet.addAsset(await assetDatabase.findByName('Turkish Lira'), 1500, 0.13)
-    // await portfolioManagerDb.save([samedLongTerm, someonesPortfolio, samedMainWallet])
-    // console.log(await portfolioManagerDb.load())
-
-
-    // add new users, then save them into db
-    // const armagan = new Investor(undefined, 'Armagan', 'xyz@nimble.dev', 'pwd')
-    // const omer = new Investor(undefined, 'Omer', 'omer@gmail.com', 'pwd')
-    // await investorDatabase.save([samed, armagan, omer])
-    // console.log(await investorDatabase.load())
-
-
-    // update users in database
-    // omer.password = 'omer123'
-    // await investorDatabase.update(omer)
-    // armagan.wallets.push(someonesPortfolio)
-    // await investorDatabase.update(armagan)
-    // samed.wallets.push(samedMainWallet)
-    // await investorDatabase.update(samed)
-    // console.log(await investorDatabase.load())
-    
-
-
-    // make some transactions
-    // const samedMainPortfolio = samed.wallets[0]
-    // await samedMainPortfolio.addAsset(await assetDatabase.findByName('Bitcoin'), 1, 40500)
-    // await samedMainPortfolio.sellAsset(await assetDatabase.findByName('Bitcoin'), 0.5, 43000)
-
-    // console.log(samedMainPortfolio)
-    // console.log(samedMainPortfolio.getTransactions) 
-    // console.log(await transactionDatabase.load())
+    const txs = investors[1].wallets[0].transactions
+    console.log(await loadTransactions(txs))
 }
 
 main()
