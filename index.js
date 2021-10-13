a84d7acd5238df2b9ca9944788f1566e54945b03
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-10-14 02:46:15 +0300

    Install express & nodemon

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
+const express = require('express')
 
-const assetDb = require('./database/asset-db')
 const investorDb = require('./database/investor-db')
-const transactionDb = require('./database/transaction-db')
 
-const { createTestData } = require('./create-data')
-const { resetDb } = require('./reset-db')
-// const { loadTransactions } = require('./lib/utils')
+const app = express()
 
-async function main () {
-  await resetDb()
-  await createTestData()
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`)
+})
 
-  const assets = await assetDb.load()
+app.get('/investors', async (req, res) => {
   const investors = await investorDb.load()
-  const transactions = await transactionDb.load()
+  res.send(JSON.stringify(investors))
+})
 
-  console.log('\nLOADED : ' + assets.length + ' Assets')
-  assets.forEach(asset => {
-    console.log(asset.id + ' ' + asset.name)
-  })
-
-  console.log('\nLOADED : ' + investors.length + ' Investors')
-  investors.forEach(investor => {
-    console.log(investor.id + ' ' + investor.name)
-  })
-
-  console.log('\nLOADED : ' + transactions.length + ' Transactions')
-  transactions.forEach(tx => {
-    console.log(`${tx.id} : ${tx.amount} ${tx.asset.symbol} : ${tx.type} : $ ${tx.price}`)
-  })
-}
-
-main()
+app.listen(3000, () => {
+  console.log('listening on 3000')
+})
---
+++ old-index.js
@@ -0,0 +1,34 @@
+
+const assetDb = require('./database/asset-db')
+const investorDb = require('./database/investor-db')
+const transactionDb = require('./database/transaction-db')
+
+const { createTestData } = require('./create-data')
+const { resetDb } = require('./reset-db')
+// const { loadTransactions } = require('./lib/utils')
+
+async function main () {
+  await resetDb()
+  await createTestData()
+
+  const assets = await assetDb.load()
+  const investors = await investorDb.load()
+  const transactions = await transactionDb.load()
+
+  console.log('\nLOADED : ' + assets.length + ' Assets')
+  assets.forEach(asset => {
+    console.log(asset.id + ' ' + asset.name)
+  })
+
+  console.log('\nLOADED : ' + investors.length + ' Investors')
+  investors.forEach(investor => {
+    console.log(investor.id + ' ' + investor.name)
+  })
+
+  console.log('\nLOADED : ' + transactions.length + ' Transactions')
+  transactions.forEach(tx => {
+    console.log(`${tx.id} : ${tx.amount} ${tx.asset.symbol} : ${tx.type} : $ ${tx.price}`)
+  })
+}
+
+main()
