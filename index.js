aadd76b9f9c3d57536d83ac5f3615f2edb8e921b
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-11-01 01:06:03 +0300

    Add login and register pages & routes

--- index.js
+++ index.js
@@ -12,8 +12,22 @@ app.get('/', (req, res) => {
 app.get('/investors', async (req, res) => {
   const investors = await investorDb.load()
   res.render('investors', { investors })
+app.get('/assets', async (req, res) => {
+  const assets = await assetDb.load()
+  console.log('allah')
+  res.render('assets', { assets: assets })
+})
+
+app.get('/asset/:id', async (req, res) => {
+  const id = req.params.id
+  res.render('login', { id: id })
+})
+
+app.route('/hello/:num', async (req, res) => {
+  res.send('hello ' + req.params.num)
 })
 
 app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('listening on 3000')
 })
