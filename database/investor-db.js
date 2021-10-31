86efe0c8c5fb3c3b4895e86b458631018e0539dd
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-11-01 01:05:13 +0300

    Add findByEmail function to investor-db

--- database/investor-db.js
+++ database/investor-db.js
@@ -5,6 +5,11 @@ class InvestorDatabase extends BaseDatabase {
   constructor () {
     super(Investor)
   }
+
+  async findByEmail (email) {
+    const objects = await this.load()
+    return objects.find(o => o.email == email)
+  }
 }
 
 module.exports = new InvestorDatabase()
