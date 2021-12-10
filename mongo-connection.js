4a09d3401dfffc232be35150ee65ecd6f6b862c5
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-12-10 04:54:22 +0300

    Add mongodb connection

---
+++ mongo-connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const assetDb = require('./database/asset-db');
+
+(async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/test')
+    console.log('MONGODB: Connected!')
+  } catch (err) {
+    console.error('MONGODB: No Connection!\n', err)
+  }
+
+  await assetDb.insert({ name: 'Coin', symbol: 'c', type: 'allah' })
+})()
