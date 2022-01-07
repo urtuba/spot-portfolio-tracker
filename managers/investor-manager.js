45dd2dda85e80b85a6af97c766f65381024f2c75
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2022-01-08 02:05:51 +0300

    Update investor-manager.js

--- managers/investor-manager.js
+++ managers/investor-manager.js
@@ -1,41 +1,53 @@
 const investorDb = require('../database/investor-db')
 const Wallet = require('../models/wallet')
+const bcrypt = require('bcrypt')
 
 class InvestorManager {
   async createWallet (investor, walletName) {
     const newWallet = new Wallet(undefined, walletName)
     investor.wallets.push(newWallet)
-    await investorDb.update(investor)
+    await investorDb.update(investor.id, investor)
   }
 
-  getWallet (investor, walletName) {
-    const wallet = investor.wallets.find(w => (walletName == w.name))
+  getWallet (userID, walletName) {
+    const investor = investorDb.findById(userID)
+    if (investor == undefined) { throw Error(`Investor with id ${userID} not found`) }
+
+    const wallet = investor.wallets.find(wallet => {
+      return wallet.name == walletName
+    })
+    if (wallet == undefined) { throw Error(`Wallet with name ${walletName} not found`) }
+
     return wallet
   }
 
-  async removeWallet (investor, walletName, savePNL = true) {
-    const wallet = this.getWallet(investor, walletName)
+  async removeWallet (userId, walletName, savePNL = true) {
+    const investor = investorDb.findById(userId)
+    if (investor == undefined) { throw Error(`Investor with id ${userId} not found`) }
+
+    const wallet = this.getWallet(userId, walletName)
     if (wallet == undefined) {
       throw Error(`${this.name} ${this.removeWallet.name}: Wallet not found.`)
     } else if (wallet.name == 'Main') {
       throw Error(`${this.name} ${this.removeWallet.name}: Main wallet can not be deleted.`)
     }
 
-    if (savePNL) wallet.pastPNL += wallet.totalPNL
-    const walletIdx = investor.wallets.indexOf(wallet)
-    investor.wallets.splice(walletIdx, 1)
+    if (savePNL) investor.pastPNL += wallet.totalPNL
+    investor.wallets = investor.wallets.filter(w => w.name != walletName)
 
-    await investorDb.update(investor)
+    return investorDb.update(investor._id, investor)
   }
 
-  async updatePassword (investor, newPwd) {
-    investor.password = newPwd
-    await investorDb.update(investor)
+  async updatePassword (investorId, newPwd) {
+    const investor = await investorDb.findById(investorId)
+    investor.password = await bcrypt.hash(newPwd, 10)
+    return investorDb.update(investor._id, investor)
   }
 
-  async updateEmail (investor, newEmail) {
+  async updateEmail (investorId, newEmail) {
+    const investor = await investorDb.findById(investorId)
     investor.email = newEmail
-    await investorDb.update(investor)
+    return investorDb.update(investor)
   }
 }
 
